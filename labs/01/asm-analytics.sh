0155622e02463a725af59a6048402a48ee4153fd
José Carlos Peñuelas Armenta <jose18_carl@hotmail.com>
2019-01-27 15:19:25 -0600

    A01113339-homework-01
    Signed-off-by: José Carlos Peñuelas Armenta <jose18_carl@hotmail.com>

---
+++ labs/01/asm-analytics.sh
@@ -0,0 +1,44 @@
+objdump -d ./simple_foo > output.txt
+
+declare -A instructions
+declare -A functions
+
+COUNTER=0
+MATCHES=0
+INFUNCTION=false
+while read line; do
+    if [[ $INFUNCTION == true ]]; then
+        if [[ -z $line ]]; then
+            INFUNCTION=false
+        else
+            set $line
+            ACTIONREAD=false
+            for word in $line; do
+                if [[ $ACTIONREAD == false ]] && [[ $word =~ ^[a-z][a-z][a-z][a-z]*$ ]] || [[ $word == "je" ]]; then
+                    instructions[$word]=$((instructions[$word] + 1))
+                    ACTIONREAD=true
+                    COUNTER=$((COUNTER + 1))
+                fi
+            done
+        fi   
+    elif [[ $line =~ ^[a-z0-9].*:$ ]]; then
+        ADDRESS="${line:0:16}"
+        FUNCTION="${line:18:-2}"
+
+        functions[$FUNCTION]=$ADDRESS
+        
+        INFUNCTION=true
+
+        MATCHES=$((MATCHES + 1))
+    fi    
+done < output.txt
+
+printf "You have %d kind of instructions in this object file:\n" "$COUNTER"
+for instruction in "${!instructions[@]}"; do
+    printf "\t%-6s\t: Executed %3s times\n" "$instruction" "${instructions[$instruction]}" 
+done
+
+printf "You have %d functions:\n" "$MATCHES"
+for function in "${!functions[@]}"; do
+    printf "\t%-21s\t: Located at %s addr\n" "$function" "${functions[$function]}" 
+done
